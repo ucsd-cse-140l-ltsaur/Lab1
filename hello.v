a5cf19cbfeafc321694da1d14a5fc8eb355f6184
ltsaur <48870997+ltsaur@users.noreply.github.com>
2019-03-23 23:54:32 -0700

    Add files via upload

---
+++ hello.v
@@ -0,0 +1,39 @@
+// --------------------------------------------------------------------
+// >>>>>>>>>>>>>>>>>>>>>>>>> COPYRIGHT NOTICE <<<<<<<<<<<<<<<<<<<<<<<<<
+// --------------------------------------------------------------------
+// Copyright (c) 2019 by UCSD CSE 140L
+// --------------------------------------------------------------------
+//
+// Permission:
+//
+//   This code for use in UCSD CSE 140L.
+//   It is synthesisable for Lattice iCEstick 40HX.  
+//
+// Disclaimer:
+//
+//   This Verilog source code is intended as a design reference
+//   which illustrates how these types of functions can be implemented.
+//   It is the user's responsibility to verify their design for
+//   consistency and functionality through the use of formal
+//   verification methods.  
+//
+// --------------------------------------------------------------------
+//           
+//                     Lih-Feng Tsaur
+//                     UCSD CSE Department
+//                     9500 Gilman Dr, La Jolla, CA 92093
+//                     U.S.A
+//
+// --------------------------------------------------------------------
+//
+// Revision History : 0.0
+
+module hello_world ;
+
+initial begin
+ $display ( "Hello World by YOUR OWN NAME" );
+ #10 $finish;
+end
+
+endmodule // End of Module hello_world
+
